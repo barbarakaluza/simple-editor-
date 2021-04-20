1722854fbfc42b7dfaf04036155c11dc64f39e4b
Barbara Kałuża <basianiemczyk1212@gmail.com>
2021-04-20 10:55:14 +0200

    end of work

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,28 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load = document.querySelector('.load--js');
+
+const currentValue = localStorage.getItem('entry');
+
+if(currentValue) {
+    document.querySelector('.info--js').innerHTML = '🚩';
+}
+
+save.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.setItem('entry', textarea.value);
+    if(textarea.value) {
+        document.querySelector('.info--js').innerHTML = '🚩';
+    }
+    else {
+        document.querySelector('.info--js').innerHTML = '';
+    }
+})
+
+load.addEventListener('click', (e) => {
+    e.preventDefault();
+    textarea.value = localStorage.getItem('entry');
+})
